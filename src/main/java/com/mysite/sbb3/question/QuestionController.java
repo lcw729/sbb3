50f94f2fedf4ec59a275621500b4fdf38589a65a
lcw729 <lcw729729@gmail.com>
2024-04-02 20:18:30 +0900

    :pencil2: 질문 등록 기능 추가하기 - 1

--- src/main/java/com/mysite/sbb3/question/QuestionController.java
+++ src/main/java/com/mysite/sbb3/question/QuestionController.java
@@ -3,10 +3,7 @@ package com.mysite.sbb3.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,5 +38,18 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(){
+        return "question_form";
+    }
+    @PostMapping("/create")
+    public String questionCreate(
+            @RequestParam(name = "subject") String subject,
+            @RequestParam(name = "content") String content) {
+        System.out.println("create question");
+        questionService.create(subject, content);
+        return "redirect:/question/list";
     }
 }
--- src/main/java/com/mysite/sbb3/question/QuestionService.java
+++ src/main/java/com/mysite/sbb3/question/QuestionService.java
@@ -4,6 +4,8 @@ import com.mysite.sbb3.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.zip.DataFormatException;
@@ -24,5 +26,13 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
