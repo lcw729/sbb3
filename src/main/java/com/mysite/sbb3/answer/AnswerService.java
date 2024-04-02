9fd6e21b8d3aa2fb3b3f2fd7435e2be9c9516af1
lcw729 <lcw729729@gmail.com>
2024-04-02 13:38:52 +0900

    :pencil2: POST방식으로 답변 생성하기

---
+++ src/main/java/com/mysite/sbb3/answer/AnswerService.java
@@ -0,0 +1,26 @@
+package com.mysite.sbb3.answer;
+
+import com.mysite.sbb3.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
